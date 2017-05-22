976f61b016c144160c0eebc7c13c03ef30eed405
BlisS <contacto@fixter.org>
2017-05-22 14:35:44 -0500

    ready to froggy

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, View, Image } from "react-native";
 import {
   Button,
   Text,
@@ -14,10 +14,15 @@ import {
   Icon,
   Right
 } from "native-base";
+import bg from '../assets/bg.png';
+import logo from '../assets/rata.png';
+
+
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
+                    <Image source={bg} style={styles.imageContainer}>
       <Container>
         <Header>
           <Left>
@@ -34,13 +39,21 @@ export default class HomeScreen extends React.Component {
           <Right />
         </Header>
         <Content padder>
-          <Card>
-            <CardItem>
-              <Body>
-                <Text>Chat App to talk some awesome people!</Text>
-              </Body>
-            </CardItem>
-          </Card>
+          
+
+            <View style={{alignItems: 'center', marginTop: 50, backgroundColor: 'transparent'}}>
+            <Text >IT Info</Text>
+            </View>
+            <View style={styles.logoContainer}>
+            <Image 
+            resizeMode="contain"
+            source={logo}
+            style={styles.logo}
+            />
+            </View>
+
+
+          
           <Button
             full
             rounded
@@ -48,7 +61,7 @@ export default class HomeScreen extends React.Component {
             style={{ marginTop: 10 }}
             onPress={() => this.props.navigation.navigate("Chat")}
           >
-            <Text>Chat With People</Text>
+            <Text>Chat con tus compañeros</Text>
           </Button>
           <Button
             full
@@ -57,10 +70,49 @@ export default class HomeScreen extends React.Component {
             style={{ marginTop: 10 }}
             onPress={() => this.props.navigation.navigate("Profile")}
           >
-            <Text>Goto Profiles</Text>
+            <Text>Ir al Mapa</Text>
           </Button>
         </Content>
       </Container>
+      
+                  </Image>
     );
   }
 }
+
+
+import { StyleSheet, Dimensions, Platform } from 'react-native';
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
+const styles = StyleSheet.create({
+    imageContainer: {
+        flex: 1,
+        width: null,
+        height: null
+    },
+    logoContainer: {
+//        flex: 1,
+        marginTop: deviceHeight / 20,
+        marginBottom: 10,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+//        flexDirection: 'row'
+    },
+    logo: {
+//        position: 'absolute',
+//        left: (Platform.OS === 'android') ? 40 : 50,
+//        top: (Platform.OS === 'android') ? 35 : 60,
+//        width: deviceWidth / 5,
+        height: 300,
+//        flex: 1,
+//        width: 150
+        
+        
+    },
+    text: {
+        color: '#D8D8D8',
+        bottom: 6,
+        fontSize: 45
+    }
+    
+});
